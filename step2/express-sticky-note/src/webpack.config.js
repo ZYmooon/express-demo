b649ed3ca4bf331c8838a22a8224729235945eae
ZYmooom <1130192385@qq.com>
2018-02-28 07:04:17 +0800

    addfile

---
+++ step2/express-sticky-note/src/webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack')
+var path = require('path')
+// var ExtractTextPlugin = require('extract-text-webpack-plugin')
+// var autoprefixer = require('autoprefixer')
+
+module.exports = {
+    entry: path.join(__dirname, "js/app/index.js"),
+    output: {
+        path: path.join(__dirname, "../public/js"),
+        filename: "index.js"
+      },
+    module: {
+        rules: [{
+            test: /\.less$/,
+            use: ["style-loader","css-loader", "less-loader",]
+        }]
+    },
+    resolve: {
+        alias: {
+            jquery: path.join(__dirname, "js/lib/jquery-3.3.1.min"),
+            mod: path.join(__dirname, "js/mod"),
+            less: path.join(__dirname, "less")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery"
+        }),
+    //     // new ExtractTextPlugin("css/index.css"),
+    //     // new webpack.LoaderOptionsPlugin({
+    //     //     options: {
+    //     //         postcss: [
+    //     //             autoprefixer(),
+    //     //         ]
+    //     //     }
+    //     // })
+    //     // new webpack.optimize.UglifyJsPlugin({
+    //     //     compress: {
+    //     //         warnings: false,
+    //     //     },
+    //     //     output: {
+    //     //         comments: false,
+    //     //     },
+    //     // }),
+    ]
+};
